d9aead166945740fbcc393cc9f876f98cd4785f7
josejavierpaez <josejavierpaez@hotmail.com>
2022-04-05 22:18:04 -0400

    add flight schema

--- src/flight/flight.module.ts
+++ src/flight/flight.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FLIGHT } from 'src/common/models/intex.models';
+import { PassengerModule } from 'src/passenger/passenger.module';
 import { FlightController } from './flight.controller';
 import { FlightService } from './flight.service';
+import { FlightSchema } from './schema/flght.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: FLIGHT.name,
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        useFactory: () => FlightSchema.plugin(require('mongoose-autopopulate')),
+      },
+    ]),
+    PassengerModule,
+  ],
   controllers: [FlightController],
-  providers: [FlightService]
+  providers: [FlightService],
 })
 export class FlightModule {}
